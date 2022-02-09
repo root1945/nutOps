69a1f4de7ac37e289f0af73c708ed2460cc7f6ea
Victor Alencar <munternl13@gmail.com>
2022-02-09 14:23:14 -0300

    config jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const config = {
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
+  coverageDirectory: "coverage",
+  testEnvironment: "node",
+  preset: "@shelf/jest-mongodb",
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+};
+
+module.exports = config;
