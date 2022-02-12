98775aaf1a482eae11cbebf3e79f2f74bf9e2027
Victor Alencar <munternl13@gmail.com>
2022-02-12 18:47:47 -0300

    test should be able to create a new user

---
+++ src/modules/user/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,28 @@
+import { ICreateUserDTO } from "@modules/user/dtos/ICreateUserDTO";
+
+import "reflect-metadata";
+import { UsersRepository } from "../../repositories/implementations/UsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+let createUserUseCase: CreateUserUseCase;
+let usersRepository: UsersRepository;
+
+describe("Authenticate User", () => {
+  beforeEach(() => {
+    usersRepository = new UsersRepository();
+    createUserUseCase = new CreateUserUseCase(usersRepository);
+  });
+
+  it("Should be able to create a new user", async () => {
+    const user: ICreateUserDTO = {
+      name: "Vitoria",
+      email: "vitoria@gmail.com",
+      phone: "11956585586",
+      password: "valeria54",
+    };
+
+    const userCreated = await createUserUseCase.execute(user);
+
+    expect(userCreated).toHaveProperty("id");
+  });
+});
